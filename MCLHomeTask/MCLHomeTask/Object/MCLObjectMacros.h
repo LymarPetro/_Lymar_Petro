16e759db06a3a0f74c3017f13df20853c0f07721
Lymar Petro <Lymarpiter@gmail.com>
2015-06-28 15:52:38 +0300

    ObjectMacros done

---
+++ MCLHomeTask/MCLHomeTask/Object/MCLObjectMacros.h
@@ -0,0 +1,13 @@
+//
+//  MCLObjectMacros.h
+//  MCLHomeTask
+//
+//  Created by Велес Тяжеловоз on 28.06.15.
+//  Copyright (c) 2015 MechanicalСrabLab. All rights reserved.
+//
+
+#ifndef MCLHomeTask_MCLObjectMacros_h
+#define MCLHomeTask_MCLObjectMacros_h
+
+
+#endif
