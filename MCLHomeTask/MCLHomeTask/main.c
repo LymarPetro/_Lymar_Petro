58f0cf3aa0512597862ca7c3bb69c9d193e48150
Lymar Petro <Lymarpiter@gmail.com>
2015-06-28 15:16:55 +0300

    empty commit

---
+++ MCLHomeTask/MCLHomeTask/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MCLHomeTask
+//
+//  Created by Велес Тяжеловоз on 28.06.15.
+//  Copyright (c) 2015 MechanicalСrabLab. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
