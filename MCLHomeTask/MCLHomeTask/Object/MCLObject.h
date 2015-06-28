dc69df63e11acc3e755bbdb23be8302a38242b0a
Lymar Petro <Lymarpiter@gmail.com>
2015-06-28 15:48:59 +0300

    File Object done

--- MCLHomeTask/MCLHomeTask/Object/MCLObject.h
+++ MCLHomeTask/MCLHomeTask/Object/MCLObject.h
@@ -11,4 +11,29 @@
 
 #include <stdio.h>
 
+typedef void (*IDPObjectDeallocatorCallback)(void *);
+
+typedef struct {
+    uint64_t _referenceCount;
+    IDPObjectDeallocatorCallback _deallocator;
+} IDPObject;
+
+extern
+void *__IDPObjectCreate(size_t objectSize, IDPObjectDeallocatorCallback deallocateCallback);
+
+#define IDPObjectCreateOfType(type) \
+__IDPObjectCreate(sizeof(type), (IDPObjectDeallocatorCallback)__##type##Deallocate)
+
+extern
+void *IDPObjectRetain(void *object);
+
+extern
+void IDPObjectRelease(void *object);
+
+extern
+uint64_t IDPObjectGetReferenceCount(void *object);
+
+extern
+void __IDPObjectDeallocate(void *object);
+
 #endif /* defined(__MCLHomeTask__MCLObject__) */
