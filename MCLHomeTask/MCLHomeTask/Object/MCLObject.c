1d564e7fb502a7f86ff1865103d24ec764edbf49
Lymar Petro <Lymarpiter@gmail.com>
2015-06-28 15:47:04 +0300

    File Object done

---
+++ MCLHomeTask/MCLHomeTask/Object/MCLObject.c
@@ -0,0 +1,9 @@
+//
+//  MCLObject.c
+//  MCLHomeTask
+//
+//  Created by Велес Тяжеловоз on 28.06.15.
+//  Copyright (c) 2015 MechanicalСrabLab. All rights reserved.
+//
+
+#include "MCLObject.h"
---
+++ MCLHomeTask/MCLHomeTask/Object/MCLObject.h
@@ -0,0 +1,14 @@
+//
+//  MCLObject.h
+//  MCLHomeTask
+//
+//  Created by Велес Тяжеловоз on 28.06.15.
+//  Copyright (c) 2015 MechanicalСrabLab. All rights reserved.
+//
+
+#ifndef __MCLHomeTask__MCLObject__
+#define __MCLHomeTask__MCLObject__
+
+#include <stdio.h>
+
+#endif /* defined(__MCLHomeTask__MCLObject__) */
